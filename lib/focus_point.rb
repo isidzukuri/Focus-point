2435cc71b8d5da19288c9cbb1f1de300f8c3c678
isidzukuri <axesigon@gmail.com>
2019-04-08 23:10:58 +0300

    better if notation

--- lib/focus_point.rb
+++ lib/focus_point.rb
@@ -1,25 +1,24 @@
 class FocusPoint
+  attr_reader :frame_data, :frame_width, :frame_height
 
-  attr_reader  :frame_data, :frame_width, :frame_height
-
-  def initialize params
+  def initialize(params)
     @img_width = params[:img_width]
     @img_height = params[:img_height]
     # - if frame size > img size change frame size proportionaly
     frame_params(params[:frame_width].to_f, params[:frame_height].to_f)
   end
 
-  def coords_for_crop_around params
+  def coords_for_crop_around(params)
     @point_x = params[:x]
     @point_y = params[:y]
-    validate_point()
+    validate_point
 
-    x1, y1, x2, y2 = draw_frame()
-    @frame_data = { 
-      x:x1, 
-      y:y1, 
-      width:(x2 - x1), 
-      height:(y2 - y1)
+    x1, y1, x2, y2 = draw_frame
+    @frame_data = {
+      x: x1,
+      y: y1,
+      width: (x2 - x1),
+      height: (y2 - y1)
     }
     enlarge_frame(x1, y1, x2, y2)
 
@@ -27,58 +26,57 @@ class FocusPoint
   end
 
   def draw_frame
-    frame_half_w = (frame_width/2).to_i
+    frame_half_w = (frame_width / 2).to_i
     frame_x_left = point_x - frame_half_w
-    frame_x_right = point_x + frame_half_w 
+    frame_x_right = point_x + frame_half_w
 
-    # - if frame border dont fit in image adjust it 
+    # - if frame border dont fit in image adjust it
     if frame_x_left < 0
-      frame_x_right += -(frame_x_left) 
+      frame_x_right += -frame_x_left
       frame_x_left = 0
     end
 
     if frame_x_right > img_width
       diff = frame_x_right - img_width
-      frame_x_left -= frame_x_right - img_width 
+      frame_x_left -= frame_x_right - img_width
       frame_x_right = img_width
     end
 
-
-    frame_half_h = (frame_height/2).to_i
+    frame_half_h = (frame_height / 2).to_i
     frame_y_top = point_y - frame_half_h
     frame_y_bottom = point_y + frame_half_h
 
     if frame_y_top < 0
-      frame_y_bottom += -(frame_y_top) 
+      frame_y_bottom += -frame_y_top
       frame_y_top = 0
     end
 
     if frame_y_bottom > img_height
-      frame_y_top -= frame_y_bottom - img_height 
+      frame_y_top -= frame_y_bottom - img_height
       frame_y_bottom = img_height
     end
 
-    return frame_x_left, frame_y_top, frame_x_right, frame_y_bottom
+    [frame_x_left, frame_y_top, frame_x_right, frame_y_bottom]
   end
 
-  def enlarge_frame x1, y1, x2, y2
+  def enlarge_frame(x1, y1, x2, y2)
     # - if frame size < img size and gap between frame border and image border present increase frame size proportionaly
-    if x1 > 0 && x2 < img_width && y1 > 0 && y2 < img_height 
+    if x1 > 0 && x2 < img_width && y1 > 0 && y2 < img_height
       gaps_percentage = [
-        x1.to_f/frame_data[:width], 
-        y1.to_f/frame_data[:height], 
-        (img_width - x2).to_f/frame_data[:width], 
-        (img_height - y2).to_f/frame_data[:height]
+        x1.to_f / frame_data[:width],
+        y1.to_f / frame_data[:height],
+        (img_width - x2).to_f / frame_data[:width],
+        (img_height - y2).to_f / frame_data[:height]
       ]
       ratio = gaps_percentage.min
       pixels_x = (frame_data[:width].to_f * ratio).to_i
       pixels_y = (frame_data[:height].to_f * ratio).to_i
-      
+
       frame_data[:x] -= pixels_x
-      frame_data[:width] += pixels_x*2
+      frame_data[:width] += pixels_x * 2
 
       frame_data[:y] -= pixels_y
-      frame_data[:height] += (pixels_y*2).to_i 
+      frame_data[:height] += (pixels_y * 2).to_i
     end
   end
 
@@ -87,24 +85,30 @@ class FocusPoint
   attr_accessor :img_width, :img_height, :point_x, :point_y, :frame_data
 
   def validate_point
-    raise "Point outside the map." if !point_x || !point_y || point_x < 0 || point_x > img_width || point_y < 0 || point_y > img_height
+    if !point_x ||
+       !point_y ||
+       point_x < 0 ||
+       point_x > img_width ||
+       point_y < 0 ||
+       point_y > img_height
+      raise 'Point outside the map.'
+    end
   end
 
-  def frame_params width, height
+  def frame_params(width, height)
     if width > img_width
-      w_ratio = width/img_width
+      w_ratio = width / img_width
       width = img_width
-      height = height/w_ratio
+      height /= w_ratio
     end
 
     if height > img_height
-      h_ratio = height/img_height
+      h_ratio = height / img_height
       height = img_height
-      width = width/h_ratio
+      width /= h_ratio
     end
 
     @frame_width = width.to_i
-    @frame_height = height.to_i    
+    @frame_height = height.to_i
   end
-
 end
--- spec/focus_point_spec.rb
+++ spec/focus_point_spec.rb
@@ -1,94 +1,82 @@
-require_relative("../lib/focus_point.rb")
-
+require_relative('../lib/focus_point.rb')
 
 describe FocusPoint do
-  let(:params){ {img_width: 1000, img_height: 1000, frame_height: 200, frame_width:300} }
-  let(:item){ FocusPoint.new(params) }
-
+  let(:params) { { img_width: 1000, img_height: 1000, frame_height: 200, frame_width: 300 } }
+  let(:item) { FocusPoint.new(params) }
 
   describe '#initialize' do
-
     it 'checks frame params and resize it if too big' do
-      prms = {img_width: 1000, img_height: 1000, frame_height: 10000, frame_width:10000}
+      prms = { img_width: 1000, img_height: 1000, frame_height: 10_000, frame_width: 10_000 }
       instance = FocusPoint.new(prms)
-      expect( instance.frame_height ).to eq 1000
-      expect( instance.frame_width ).to eq 1000
-    
-      prms = {img_width: 1000, img_height: 1000, frame_height: 1000, frame_width: 1200}
-      instance = FocusPoint.new(prms)
-      expect( instance.frame_height ).to eq 833
-      expect( instance.frame_width ).to eq 1000
+      expect(instance.frame_height).to eq 1000
+      expect(instance.frame_width).to eq 1000
 
-      prms = {img_width: 1000, img_height: 1000, frame_height: 1400, frame_width: 1200}
+      prms = { img_width: 1000, img_height: 1000, frame_height: 1000, frame_width: 1200 }
       instance = FocusPoint.new(prms)
-      expect( instance.frame_height ).to eq 1000
-      expect( instance.frame_width ).to eq 857
+      expect(instance.frame_height).to eq 833
+      expect(instance.frame_width).to eq 1000
+
+      prms = { img_width: 1000, img_height: 1000, frame_height: 1400, frame_width: 1200 }
+      instance = FocusPoint.new(prms)
+      expect(instance.frame_height).to eq 1000
+      expect(instance.frame_width).to eq 857
     end
-
   end
-
-
 
   describe '#coords_for_crop_around' do
     it 'raise exception if point coordinates is not correct' do
-      expect { item.coords_for_crop_around(x:-500, y: 8000) }.to raise_error
+      expect { item.coords_for_crop_around(x: -500, y: 8000) }.to raise_error
     end
 
     it 'makes crop frame bigger if possible' do
-      prms = {img_width: 1000, img_height: 1000, frame_height: 100, frame_width: 100}
+      prms = { img_width: 1000, img_height: 1000, frame_height: 100, frame_width: 100 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 500, y: 500) ).to eq({:x=>0, :y=>0, :width=>1000, :height=>1000})
+      expect(instance.coords_for_crop_around(x: 500, y: 500)).to eq(x: 0, y: 0, width: 1000, height: 1000)
 
-      prms = {img_width: 1000, img_height: 1000, frame_height: 50, frame_width: 100}
+      prms = { img_width: 1000, img_height: 1000, frame_height: 50, frame_width: 100 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 500, y: 500) ).to eq({:x=>0, :y=>250, :width=>1000, :height=>500})
+      expect(instance.coords_for_crop_around(x: 500, y: 500)).to eq(x: 0, y: 250, width: 1000, height: 500)
 
-      prms = {img_width: 1000, img_height: 1000, frame_height: 100, frame_width: 50}
+      prms = { img_width: 1000, img_height: 1000, frame_height: 100, frame_width: 50 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 500, y: 500) ).to eq({:x=>250, :y=>0, :width=>500, :height=>1000})
+      expect(instance.coords_for_crop_around(x: 500, y: 500)).to eq(x: 250, y: 0, width: 500, height: 1000)
 
-
-      prms = {img_width: 500, img_height: 1000, frame_height: 100, frame_width: 100}
+      prms = { img_width: 500, img_height: 1000, frame_height: 100, frame_width: 100 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 250, y: 500) ).to eq({:x=>0, :y=>250, :width=>500, :height=>500})
+      expect(instance.coords_for_crop_around(x: 250, y: 500)).to eq(x: 0, y: 250, width: 500, height: 500)
 
-      prms = {img_width: 500, img_height: 1000, frame_height: 100, frame_width: 50}
+      prms = { img_width: 500, img_height: 1000, frame_height: 100, frame_width: 50 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 250, y: 500) ).to eq({:x=>0, :y=>0, :width=>500, :height=>1000})
+      expect(instance.coords_for_crop_around(x: 250, y: 500)).to eq(x: 0, y: 0, width: 500, height: 1000)
 
-      prms = {img_width: 500, img_height: 1000, frame_height: 50, frame_width: 100}
+      prms = { img_width: 500, img_height: 1000, frame_height: 50, frame_width: 100 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 250, y: 500) ).to eq({:x=>0, :y=>375, :width=>500, :height=>250})
+      expect(instance.coords_for_crop_around(x: 250, y: 500)).to eq(x: 0, y: 375, width: 500, height: 250)
 
+      prms = { img_width: 1500, img_height: 1000, frame_height: 100, frame_width: 1000 }
+      instance = FocusPoint.new(prms)
+      expect(instance.coords_for_crop_around(x: 750, y: 500)).to eq(x: 0, y: 425, width: 1500, height: 150)
 
-      prms = {img_width: 1500, img_height: 1000, frame_height: 100, frame_width: 1000}
+      prms = { img_width: 1500, img_height: 1000, frame_height: 500, frame_width: 250 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 750, y: 500) ).to eq({:x=>0, :y=>425, :width=>1500, :height=>150})
-    
-      prms = {img_width: 1500, img_height: 1000, frame_height: 500, frame_width: 250}
-      instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 750, y: 500) ).to eq({:x=>500, :y=>0, :width=>500, :height=>1000})
-      
+      expect(instance.coords_for_crop_around(x: 750, y: 500)).to eq(x: 500, y: 0, width: 500, height: 1000)
 
-      prms = {img_width: 1500, img_height: 1000, frame_height: 250, frame_width: 250}
+      prms = { img_width: 1500, img_height: 1000, frame_height: 250, frame_width: 250 }
       instance = FocusPoint.new(prms)
-      expect( instance.coords_for_crop_around(x: 750, y: 500) ).to eq({:x=>250, :y=>0, :width=>1000, :height=>1000})
-      
+      expect(instance.coords_for_crop_around(x: 750, y: 500)).to eq(x: 250, y: 0, width: 1000, height: 1000)
     end
 
-    it 'calls draw_frame method which returns frame border`s values' do 
+    it 'calls draw_frame method which returns frame border`s values' do
       expect(item).to receive(:draw_frame).and_call_original
-      expect( item.coords_for_crop_around(x: 500, y: 500) ).to eq({:x=>0, :y=>167, :width=>1000, :height=>666})
+      expect(item.coords_for_crop_around(x: 500, y: 500)).to eq(x: 0, y: 167, width: 1000, height: 666)
     end
-
 
     it 'will move frame if it crosses image`s borders' do
-      expect( item.coords_for_crop_around(x: 0, y: 0) ).to eq({:x=>0, :y=>0, :width=>300, :height=>200})
-      expect( item.coords_for_crop_around(x: 50, y: 500) ).to eq({:x=>0, :y=>400, :width=>300, :height=>200})
-      expect( item.coords_for_crop_around(x: 1000, y: 1000) ).to eq({:x=>700, :y=>800, :width=>300, :height=>200})
-      expect( item.coords_for_crop_around(x: 950, y: 950) ).to eq({:x=>700, :y=>800, :width=>300, :height=>200})
-      expect( item.coords_for_crop_around(x: 900, y: 500) ).to eq({:x=>700, :y=>400, :width=>300, :height=>200})
+      expect(item.coords_for_crop_around(x: 0, y: 0)).to eq(x: 0, y: 0, width: 300, height: 200)
+      expect(item.coords_for_crop_around(x: 50, y: 500)).to eq(x: 0, y: 400, width: 300, height: 200)
+      expect(item.coords_for_crop_around(x: 1000, y: 1000)).to eq(x: 700, y: 800, width: 300, height: 200)
+      expect(item.coords_for_crop_around(x: 950, y: 950)).to eq(x: 700, y: 800, width: 300, height: 200)
+      expect(item.coords_for_crop_around(x: 900, y: 500)).to eq(x: 700, y: 400, width: 300, height: 200)
     end
   end
-
 end
